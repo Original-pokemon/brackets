4de746752759f72aec0882cbc6c870cd2c4c2ecf
original-pokemon <eremkin-02@mail.ru>
2021-03-11 20:10:16 +0300

    feat: add check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const configMap = bracketsConfig.map(b => b.join('') );
+
+  for (let i = 0; i < configMap.length; i++) {
+    
+    if (str.includes( configMap[i] ) ) {
+      str = str.replace(configMap[i], '');
+      i = -1;
+    }
+  }
+  return !str;
 }
